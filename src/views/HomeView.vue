f9f33b1a71ff61cc4a0f5c69ac5c70f2ec92d713
jim <f0920515972@gmail.com>
2024-12-17 12:41:23 +0800

    refactor: 將 HomeHeader 路徑從 /components 移至 /components/header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import HomeHeader from '@/components/HomeHeader.vue';
+import HomeHeader from '@/components/header/HomeHeader.vue';
+import homeMain from '@/components/main/HomeMain.vue';
 </script>
 
 <template>
   <main>
     <HomeHeader/>
+    <homeMain/>
   </main>
 </template>
